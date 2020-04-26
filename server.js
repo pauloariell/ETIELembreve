e79e343ae60af4874e3090b4209c8f473cdd7380
PAC <PAC@NBPAC>
2020-04-25 21:06:20 -0300

    deploy tentativa 5

---
+++ server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//serve only the static files form the dist directory
+//Replace the 'dist/<to_your_project_name>'
+app.use(express.static(__dirname + 'dist/Etielengenharia'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + 'dist/Etielengenharia/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
