e85606ef5bb55e37c45840e7f4a78eca3181cd90
PAC <PAC@NBPAC>
2020-04-25 23:22:10 -0300

    corrigindo caminho do index.html

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 
 //serve only the static files form the dist directory
 //Replace the 'dist/<to_your_project_name>'
-app.use(express.static(__dirname + 'dist/Etielengenharia'));
+app.use(express.static(__dirname + '/dist/Etielengenharia'));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'dist/Etielengenharia/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Etielengenharia/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
