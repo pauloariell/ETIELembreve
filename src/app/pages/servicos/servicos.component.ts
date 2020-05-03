46f0626b20afa35b044afcc1dd0cf315c81b5960
PAC <PAC@NBPAC>
2020-05-03 03:15:41 -0300

    Montando tela de servicos

--- src/app/pages/servicos/servicos.component.ts
+++ src/app/pages/servicos/servicos.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servicos',
+  selector: 'ee-servicos',
   templateUrl: './servicos.component.html',
   styleUrls: ['./servicos.component.scss']
 })
 export class ServicosComponent implements OnInit {
+  servicos = [
+    {
+      img: 'assets/img/servico-1.jpg',
+      titulo: 'Perícias de Engenharia',
+      descricao:'Judiciais, extrajudiciais, cautelares, arbitrárias, em Edificações, em ações reais imobiliárias, etc'
+    },
+    {
+      img: 'assets/img/no-image.jpg',
+      titulo: 'Avaliações de Imóveis - Urbanos e Rurais',
+      descricao:'Garantias, Patrimoniais, Desapropriação, Revisional de Aluguel, Renovatória de contrato de locação, etc'
+    }
+  ];
 
   constructor() { }
-
   ngOnInit(): void {
   }
 
