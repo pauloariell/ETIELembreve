c70a4dcdac05eb310dd4051ca1d8c159d697d05e
PAC <PAC@NBPAC>
2020-05-08 00:42:32 -0300

    Page de Contato - sem comunicação com a api

--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-contato',
+  selector: 'ee-contato',
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.scss']
 })
